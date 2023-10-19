7f103c7601a189b48721f10f421f77bbb338daec
Tomas Walander <twalander@gmail.com>
2023-10-19 15:10:47 +0200

    Copy root readme to docs

--- .scripts/generate-docs.sh
+++ .scripts/generate-docs.sh
@@ -20,8 +20,12 @@ done
 rm -rf docs
 mkdir docs
 
+cp lib/README.md docs/README.md
+
 for source_file_path in ./lib/**/*.md; do
   destination_file_path="docs/${source_file_path#"./lib/"}"
+  echo $source_file_path
+  echo $destination_file_path
 
   mkdir -p "$(dirname "$destination_file_path")"
   cp "$source_file_path" "$destination_file_path"
